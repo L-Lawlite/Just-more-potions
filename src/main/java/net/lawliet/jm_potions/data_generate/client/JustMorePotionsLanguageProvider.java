931cf26a8d4dce38514401ed0b7012c0bfb42c3a
Lawliet <junirobert21@gmail.com>
2025-05-04 11:31:07 +0530

    Added decay potion
    (cherry picked from commit 0d8d68070bca2acc1499d0c19ac4848f75461dad)

--- src/main/java/net/lawliet/jm_potions/data_generate/client/JustMorePotionsLanguageProvider.java
+++ src/main/java/net/lawliet/jm_potions/data_generate/client/JustMorePotionsLanguageProvider.java
@@ -6,6 +6,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.item.alchemy.Potion;
 import net.neoforged.neoforge.common.data.LanguageProvider;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.*;
 import java.util.function.Supplier;
@@ -17,23 +18,27 @@ public class JustMorePotionsLanguageProvider extends LanguageProvider {
     public JustMorePotionsLanguageProvider(PackOutput output, String lang) {
         super(output, JustMorePotions.MODID,lang);
     }
-    protected void addPotion(DeferredRegister<Potion> key, Supplier<? extends MobEffect> mobEffect, String name) {
-        addEffect(mobEffect, name);
-        add("mobEffects." + JustMorePotions.MODID + "." + mobEffect.get().getDescriptionId(),name);
+
+    protected void addPotion(DeferredHolder<Potion,Potion> potionHolder, String name) {
+        String potionKey = potionHolder.getKey().location().getPath();
         Map<String,String> potionItemTypes = Map.ofEntries(
                 entry("splash_potion","Splash Potion of "),
                 entry("potion","Potion of "),
                 entry("lingering_potion","Lingering Potion of "),
                 entry("tipped_arrow","Arrow of ")
         );
-        Iterable<String> potions = getAllPotionsKeys(key);
 
-        for (String potion: potions) {
-            for (String potionItemType : potionItemTypes.keySet()) {
-                String keyName = "item.minecraft." + potionItemType + ".effect." + potion;
-                add(keyName,potionItemTypes.get(potionItemType) + name);
-            }}
+        for (String potionItemType : potionItemTypes.keySet()) {
+            String keyName = "item.minecraft." + potionItemType + ".effect." + potionKey;
+            add(keyName,potionItemTypes.get(potionItemType) + name);
+        }
 
+    }
+
+    protected void addPotion(Supplier<? extends MobEffect> mobEffect,DeferredHolder<Potion,Potion> potionHolder, String name) {
+        addEffect(mobEffect, name);
+        add("mobEffects." + JustMorePotions.MODID + "." + mobEffect.get().getDescriptionId(),name);
+        addPotion(potionHolder,name);
     }
 
     protected Iterable<String> getAllPotionsKeys(DeferredRegister<Potion> potionRegistry) {
@@ -44,7 +49,8 @@ public class JustMorePotionsLanguageProvider extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
-        addPotion(Registration.POTIONS,Registration.DETONATION,"Detonation");
+        addPotion(Registration.DETONATION,Registration.DETONATION_POTION,"Detonation");
+        addPotion(Registration.DECAY_POTION,"Decay");
     }
 
 
---
+++ src/main/java/net/lawliet/jm_potions/entity/effects/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package net.lawliet.jm_potions.entity.effects;
+
+import javax.annotation.ParametersAreNonnullByDefault;
